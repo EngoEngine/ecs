5577c213bf43f7d6c9a928dde96cbd6a1e288135
Jerry Caligiure <caligiure.ja@gmail.com>
2020-08-16 14:44:48 -0400

    added sort method to world in case needed for systems that dynamically update their priority

--- world_test.go
+++ world_test.go
@@ -50,11 +50,10 @@ func (s *simpleSystem) AddByInterface(i Identifier) {
 	}
 }
 
-func (s *simpleSystem) Remove(b BasicEntity) {
-}
+func (s *simpleSystem) Remove(b BasicEntity) {}
 
-func (s *simpleSystem) Update(dt float32) {
-}
+func (s *simpleSystem) Update(dt float32) {}
+
 func TestWorld_AddEntity(t *testing.T) {
 
 	type args struct {
@@ -82,5 +81,61 @@ func TestWorld_AddEntity(t *testing.T) {
 				t.Error(len(sys.entities))
 			}
 		})
+	}
+}
+
+type priorityChangeSystem struct {
+	Rank int
+}
+
+func (s *priorityChangeSystem) Priority() int {
+	return s.Rank
+}
+
+func (s *priorityChangeSystem) Add(b *BasicEntity) {}
+
+func (s *priorityChangeSystem) AddByInterface(i Identifier) {
+	obj, ok := i.(BasicFace)
+	if ok {
+		s.Add(obj.GetBasicEntity())
+	}
+}
+
+func (s *priorityChangeSystem) Remove(b BasicEntity) {}
+
+func (s *priorityChangeSystem) Update(dt float32) {}
+
+func TestWorld_SortSystems(t *testing.T) {
+	w := new(World)
+	one := priorityChangeSystem{Rank: 1}
+	w.AddSystem(&one)
+	two := priorityChangeSystem{Rank: 2}
+	w.AddSystem(&two)
+	expected := []System{
+		&two,
+		&one,
+	}
+	for idx, sys := range w.Systems() {
+		p := sys.(Prioritizer)
+		exp := expected[idx].(Prioritizer)
+		if p.Priority() != exp.Priority() {
+			t.Error("Systems were not in the correct order")
+		}
+	}
+	one.Rank = 5
+	for idx, sys := range w.Systems() {
+		p := sys.(Prioritizer)
+		exp := expected[idx].(Prioritizer)
+		if p.Priority() != exp.Priority() {
+			t.Error("Systems were switched before sort wwas called")
+		}
+	}
+	w.SortSystems()
+	for idx, sys := range w.Systems() {
+		p := sys.(Prioritizer)
+		exp := expected[len(expected)-1-idx].(Prioritizer)
+		if p.Priority() != exp.Priority() {
+			t.Error("Systems were switched before sort wwas called")
+		}
 	}
 }
