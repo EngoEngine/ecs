997ed6e32ecc7b53e44fc1f5b0829a3ae68cb8c1
Noofbiz <caligiure.ja@gmail.com>
2023-12-15 23:30:30 -0500

    fixed length lookbehind

--- .github/testing.sh
+++ .github/testing.sh
@@ -3,6 +3,6 @@
 go test -coverprofile=coverage.out -bench=. .
 go tool cover -func=coverage.out -o=coverage.out
 
-out = $(grep -Po '(?<=\(statements\)\s+)\d+.\d+%' coverage.out)
+out = $(grep -Po '(?<=\(statements\)\s\s)\d+.\d+%' coverage.out)
 
 echo "$out"
