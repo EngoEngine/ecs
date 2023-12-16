fb428b90fac153a517f5a1d1bb679a3f3b8b8030
Noofbiz <caligiure.ja@gmail.com>
2023-12-15 22:42:21 -0500

    now I know where the script runs I can start testing and checking the results

--- .github/testing.sh
+++ .github/testing.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-echo $(ls -la)
+go test -coverprofile=coverage.out -bench=. .
+go tool cover -func=coverage.out -o=coverage.out
+
+out = grep '/(?:\(statements\)\s+)(\d+.\d%)/' coverage.out
+
+echo "$out"
