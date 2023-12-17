3d9f7d18e9b3bfb5d4135c49d88d70bb99d5dd2e
Noofbiz <caligiure.ja@gmail.com>
2023-12-16 19:52:03 -0500

    using external actions instead of script since it would've been the same and this is simpler

--- .github/testing.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-go test -coverprofile=coverage.out -bench=. .
-go tool cover -func=coverage.out -o=coverage.out
-
-outputFromCoverage=$(grep -Po '(?<=\(statements\)\s\s)\d+.\d+%' coverage.out)
-
-echo "$outputFromCoverage"
